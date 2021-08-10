11ec404ba532d05f5a30a7333896531e4aabec41
tetsuya_kanamaru <67969845+tetsuya-kanamaru@users.noreply.github.com>
2021-08-10 20:45:30 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :restaurants do
+        resources :foods, only: %i[index]
+      end
+
+      resources :line_foods, only: %i[index create]
+      put 'line_food/replace', to: 'line_food#replace'
+
+      resources :orders, only: %i[create]
+    end
+  end
+end
